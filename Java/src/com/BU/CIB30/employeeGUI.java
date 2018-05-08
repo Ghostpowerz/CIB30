dc949236deabd9e5a1f6fc85ad262c7ef10207f9
gareth-gill <39086964+gareth-gill@users.noreply.github.com>
2018-05-08 13:47:46 +0100

    PUSH - employeeGUI UPDATE

--- Java/src/com/BU/CIB30/employeeGUI.java
+++ Java/src/com/BU/CIB30/employeeGUI.java
@@ -1,6 +1,8 @@
 package com.BU.CIB30;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 /**
@@ -13,20 +15,31 @@ import javax.swing.SwingUtilities;
 public class testGUI {
 	
 	private JFrame applicationFrame;
+	private JPanel employeePanel;
+	private JButton empButton1;
 	
 	private testGUI() {
 		createApplicationFrame();
+		createEmployeePanel();
+		createEmployeeButton();
 	}
 	
 	private void createApplicationFrame() {
 		applicationFrame = new JFrame("Design & Solutions Co");
-		applicationFrame.setSize(1200, 1080);
+		applicationFrame.setSize(1200, 800);
 		applicationFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		applicationFrame.setVisible(true);
 	}
 	
+	private void createEmployeePanel() {
+		employeePanel = new JPanel();
+		employeePanel.setLayout(null);
+		employeePanel.add(empButton1);
+	}
 	
-	
+	private void createEmployeeButton() {
+		
+	}
 	
 	
 	
