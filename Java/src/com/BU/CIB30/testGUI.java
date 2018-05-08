1a6279e6bfcb3834d3af8f87579beb1eb53c2fc8
gareth-gill <39086964+gareth-gill@users.noreply.github.com>
2018-05-08 13:02:10 +0100

    Add files via upload

---
+++ Java/src/com/BU/CIB30/testGUI.java
@@ -0,0 +1,22 @@
+package com.BU.CIB30;
+
+import javax.swing.JFrame;
+
+/**
+ * @author s5009373
+ * CREATE BASIC GUI
+ * FOREGROUND/BACKGROUND FORMATTING
+ * SCREENSIZE 1200x1080
+ */
+
+public class testGUI {
+	
+	private testGUI() {
+		createApplicationFrame();
+	}
+	
+	private void createApplicationFrame() {
+		applicationFrame = new JFrame("Design & Solutions Co");
+	}
+
+}
